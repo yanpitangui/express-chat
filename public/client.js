c7353741add424bceb6e7d442c77c530cae58daa
Yan Carlos Cordeiro Pitangui <yan.pitangui@cscservicos.com.br>
2019-12-17 15:12:36 -0300

    ajustes em layout

--- public/client.js
+++ public/client.js
@@ -1,105 +1,124 @@
-$( document ).ready(function() {
+ $(document).ready(function() {
   // Form submittion with new message in field with id 'm'
-  $('form').submit(function(){
-    if($('#m').val().trim() == '') return false;
-    var messageToSend = $('#m').val();
-    socket.emit('chat message', messageToSend);
+  $("form").submit(function() {
+    if (
+      $("#m")
+        .val()
+        .trim() == ""
+    )
+      return false;
+    var messageToSend = $("#m").val();
+    socket.emit("chat message", messageToSend);
     //send message to server here?
-    $('#m').val('');
+    $("#m").val("");
     return false; // prevent form submit from refreshing page
   });
-  
+
   /*global io*/
   var socket = io();
-  socket.on('user', function(data){
-  $('#num-users').text(data.currentUsers+' Usuário (s) Online');
-  var message = data.name;
-  if(data.connected) {
-    message += ' é mais um otário logado!';
-  } else {
-    message += ' é vacilão e deslogou!';
-  }
-    
-  $('#listaUsuarios').html("");
-  data.listaUsuarios.forEach((element) => {
-    $('#listaUsuarios').append($('<p>').html('<b>'+ element +'<\/b>'));
+  socket.on("user", function(data) {
+    console.log(data);
+    $("#num-users").text(data.currentUsers + " Usuário (s) Online");
+    var message = `<span style="color:${data.color}" >${data.name}</span>`;
+    if (data.connected) {
+      message += " é mais um otário logado!";
+    } else {
+      message += " é vacilão e deslogou!";
+    }
+
+    $("#listaUsuarios").html("");
+    data.listaUsuarios.forEach(element => {
+      $("#listaUsuarios").append($("<p>").html(`<b><span style="color:${element.color}" >` + element.name + "</span></b>"));
+    });
+
+    $("#messages").append($("<li>").html("<b>" + message + "</b>"));
   });
-    
-  $('#messages').append($('<li>').html('<b>'+ message +'<\/b>'));
-});
-  
-  socket.on('disconnect', function(){
-    socket.emit('disconnect');
+
+  socket.on("disconnect", function() {
+    socket.emit("disconnect");
   });
-  
-  socket.on('chat message', function(data) {
-    $('#messages').append($('<li>').html('<b>'+data.name + ': ' + data.message +'<\/b>'));
-    $('div.chat').scrollTop($('div.chat')[0].scrollHeight);
+
+  socket.on("chat message", function(data) {
+    console.log(data);
+    $("#messages").append(
+      $("<li>").html(
+        `<b><span style="color:${data.color}" >` +
+          data.name +
+          "</span>: " +
+          data.message +
+          "</b>"
+      )
+    );
+    $("div.chat").scrollTop($("div.chat")[0].scrollHeight);
     Notification.requestPermission();
     if (document.hidden) {
-      Notification.requestPermission().then(function(permission) { 
-      if(permission === 'granted') {
-        var noty = new Notification(data.name, {
-          body: data.message,
-          dir: 'auto', // or ltr, rtl
-          lang: 'EN', //lang used within the notification.
-          tag: 'notificationPopup', //An element ID to get/set the content
-          icon: 'https://cdn.glitch.com/054ae6c2-367c-4bd7-bc58-3f0aee51ecc4%2Ftoxic-xxl.png?1548695517512' //The URL of an image to be used as an icon
-        });
-        noty.onclick = function () {
+      Notification.requestPermission().then(function(permission) {
+        if (permission === "granted") {
+          var noty = new Notification(data.name, {
+            body: data.message,
+            dir: "auto", // or ltr, rtl
+            lang: "EN", //lang used within the notification.
+            tag: "notificationPopup", //An element ID to get/set the content
+            icon:
+              "https://cdn.glitch.com/054ae6c2-367c-4bd7-bc58-3f0aee51ecc4%2Ftoxic-xxl.png?1548695517512" //The URL of an image to be used as an icon
+          });
+          noty.onclick = function() {
             parent.focus();
             noty.close();
-        };
-      }
-    });
-    // do what you need
+          };
+        }
+      });
+      // do what you need
     }
-    $('#messages').linkify();
+    $("#messages").linkify();
   });
-  
-  var textarea = $('#m');
-  var typingStatus = $('#typing');
+
+  var textarea = $("#m");
+  var typingStatus = $("#typing");
   var usersTyping = [];
   var lastTypedTime = new Date(0);
   var typingDelayMillis = 3000;
 
   function refreshTypingStatus() {
-      const ut = usersTyping.map((value)=>{return value.name});
-      if(ut.length==0) {
-        typingStatus.html('');
-      }
-      else if(ut.length>=4){
-        typingStatus.html('Muita gente está escrevendo...');
-      } else {
-        const typingstring = ut.join(', ') + ' ' + (ut.length==1? 'está': 'estão') + ' escrevendo...';
-        typingStatus.html(typingstring);
-      }
+    const ut = usersTyping.map(value => {
+      return value.name;
+    });
+    if (ut.length == 0) {
+      typingStatus.html("");
+    } else if (ut.length >= 4) {
+      typingStatus.html("Muita gente está escrevendo...");
+    } else {
+      const typingstring =
+        ut.join(", ") +
+        " " +
+        (ut.length == 1 ? "está" : "estão") +
+        " escrevendo...";
+      typingStatus.html(typingstring);
+    }
   }
   function updateLastTypedTime() {
-      if(new Date().getTime() - lastTypedTime.getTime() > typingDelayMillis) {
-        socket.emit('writing', new Date());
-        lastTypedTime = new Date();
-      }
+    if (new Date().getTime() - lastTypedTime.getTime() > typingDelayMillis) {
+      socket.emit("writing", new Date());
+      lastTypedTime = new Date();
+    }
   }
 
   setInterval(refreshTypingStatus, 1000);
   textarea.keypress(updateLastTypedTime);
   textarea.blur(refreshTypingStatus);
-  
-  socket.on('user writing', function(data) {
-    console.log('evento chegou');
+
+  socket.on("user writing", function(data) {
+    console.log("evento chegou");
     usersTyping.push(data);
     refreshTypingStatus();
-    setTimeout(function(){ 
+    setTimeout(function() {
       limpaEscrevendo(data.name);
-    }, typingDelayMillis-100);
+    }, typingDelayMillis - 100);
   });
-  
+
   function limpaEscrevendo(usuario) {
     usersTyping = usersTyping.filter(function(value) {
-      return value.name!==usuario;
-      
+      return value.name !== usuario;
     });
   }
-
 });
--- server.js
+++ server.js
@@ -1,80 +1,119 @@
-'use strict';
+"use strict";
 
-const express     = require('express');
-const session     = require('express-session');
-const bodyParser  = require('body-parser');
-const fccTesting  = require('./freeCodeCamp/fcctesting.js');
-const auth        = require('./app/auth.js');
-const routes      = require('./app/routes.js');
-const mongo       = require('mongodb').MongoClient;
-const passport    = require('passport');
-const cookieParser= require('cookie-parser')
-const app         = express();
-const http        = require('http').Server(app);
-const sessionStore= new session.MemoryStore();
-const io = require('socket.io')(http);
-const cors = require('cors');
-const passportSocketIo = require('passport.socketio');
+const express = require("express");
+const session = require("express-session");
+const bodyParser = require("body-parser");
+const fccTesting = require("./freeCodeCamp/fcctesting.js");
+const auth = require("./app/auth.js");
+const routes = require("./app/routes.js");
+const mongo = require("mongodb").MongoClient;
+const passport = require("passport");
+const cookieParser = require("cookie-parser");
+const app = express();
+const http = require("http").Server(app);
+const sessionStore = new session.MemoryStore();
+const io = require("socket.io")(http);
+const cors = require("cors");
+const passportSocketIo = require("passport.socketio");
 app.use(cors());
 
 fccTesting(app); //For FCC testing purposes
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.set('view engine', 'pug')
+app.set("view engine", "pug");
 
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: true,
-  saveUninitialized: true,
-  key: 'express.sid',
-  store: sessionStore,
-}));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    key: "express.sid",
+    store: sessionStore
+  })
+);
 
 function isInArray(value, array) {
   return array.indexOf(value) > -1;
 }
 
 mongo.connect(process.env.MONGO_URI, { useNewUrlParser: true }, (err, db) => {
-    if(err) console.log('Database error: ' + err);
-  
-    auth(app, db.db());
-    routes(app, db.db());
-      
-    http.listen(process.env.PORT || 3000);
-    io.use(passportSocketIo.authorize({
-      cookieParser: cookieParser,
-      key:          'express.sid',
-      secret:       process.env.SESSION_SECRET,
-      store:        sessionStore
-    }));
-    var currentUsers = 0;
-    var listaUsuarios = [];
-    //start socket.io code  
-    io.on('connection', socket => {
-      let written = new Date();
-      if(!isInArray(socket.request.user.name, listaUsuarios)) {
-        listaUsuarios.push(socket.request.user.name);
-        currentUsers = listaUsuarios.length;
-        io.emit('user', {name: socket.request.user.name, currentUsers, connected: true, listaUsuarios: listaUsuarios});
-      }
+  if (err) console.log("Database error: " + err);
 
-      socket.on('disconnect', ()=>{
-        listaUsuarios = listaUsuarios.filter((elem)=> { return elem!==socket.request.user.name; });
-        currentUsers = listaUsuarios.length;
-        io.emit('user', {name: socket.request.user.name, currentUsers, connected: false, listaUsuarios: listaUsuarios});
+  auth(app, db.db());
+  routes(app, db.db());
+
+  http.listen(process.env.PORT || 3000);
+  io.use(
+    passportSocketIo.authorize({
+      cookieParser: cookieParser,
+      key: "express.sid",
+      secret: process.env.SESSION_SECRET,
+      store: sessionStore
+    })
+  );
+
+  function gera_cor() {
+    var hexadecimais = "0123456789ABCDEF";
+    var cor = "#";
+    // Pega um número aleatório no array acima
+    for (var i = 0; i < 6; i++) {
+      //E concatena à variável cor
+      cor += hexadecimais[Math.floor(Math.random() * 16)];
+    }
+    return cor;
+  }
+  
+  var currentUsers = 0;
+  var listaUsuarios = [];
+  //start socket.io code
+  io.on("connection", socket => {    
+    socket.request.user.color = gera_cor();
+    let written = new Date();
+    if (!isInArray(socket.request.user.name, listaUsuarios)) {
+      listaUsuarios.push({
+        name: socket.request.user.name,
+        color: socket.request.user.color
       });
-      socket.on('chat message', message => {
-        io.emit('chat message', {name: socket.request.user.name, message});
+      currentUsers = listaUsuarios.length;
+      io.emit("user", {
+        name: socket.request.user.name,
+        currentUsers,
+        connected: true,
+        listaUsuarios: listaUsuarios,
+        color: socket.request.user.color
       });
-      
-      socket.on('writing', date => {
-        socket.broadcast.emit('user writing', {name: socket.request.user.name, date: date});
+    }
+
+    socket.on("disconnect", () => {
+      listaUsuarios = listaUsuarios.filter(elem => {
+        return elem.name !== socket.request.user.name;
+      });
+      currentUsers = listaUsuarios.length;
+      io.emit("user", {
+        name: socket.request.user.name,
+        currentUsers,
+        connected: false,
+        listaUsuarios: listaUsuarios,
+        color: socket.request.user.color
+      });
+    });
+    socket.on("chat message", message => {
+      io.emit("chat message", {
+        name: socket.request.user.name,
+        message,
+        color: socket.request.user.color
       });
     });
 
-    //end socket.io code
-  
-  
+    socket.on("writing", date => {
+      socket.broadcast.emit("user writing", {
+        name: socket.request.user.name,
+        date: date
+      });
+    });
+  });
+
+  //end socket.io code
 });
